9eb4f2ac8c5edc20f02d9864e88ca1207f064cf3
PLAYERRKING <leonardomiguelsanchez@gmail.com>
2026-05-11 08:33:46 -0600

    Delete Proyecto_Vision directory

--- Proyecto_Vision/vision_planos/preprocesamiento.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-def limpiar_imagen(img):
-    # Conversión a gris y suavizado Gaussiano[cite: 2]
-    gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gris, (5, 5), 0) # El kernel de 5x5 que usas en clase[cite: 2]
-    return blur
